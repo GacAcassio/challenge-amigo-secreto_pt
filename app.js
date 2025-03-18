72b2882f1210fe8de5874355047bf629b6b30082
Elg4turu <acassiocorreia03@gmail.com>
2025-03-18 10:17:52 -0400

    Listar amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome !== "") {
+        amigos.push(nome);
+        atualizarLista();
+        input.value = ""; 
+        input.focus(); 
+    } else {
+        alert("Por favor, digite um nome");
+    }
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
