adf410a13e403e5145c375ec0d5472a478e468ae
Elg4turu <acassiocorreia03@gmail.com>
2025-03-18 10:11:24 -0400

    Agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome !== "") {
+        amigos.push(nome);
+        atualizarLista();
+        input.value = ""; 
+        input.focus(); 
+    } else {
+        alert("Por favor, digite um nome");
+    }
+}
